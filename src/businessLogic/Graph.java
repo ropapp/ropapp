8a7db589d348c64652e116d622d1d97bb70c6a1b
camilotorresmestra <juctorresme@unal.edu.co>
2018-05-12 01:46:02 -0500

    Que la lumière soit
    I just created all the packages and the classes I suppose we're going to
    use

---
+++ src/businessLogic/Graph.java
@@ -0,0 +1,12 @@
+package businessLogic;
+
+public class Graph {
+	public List nodes;
+	/**
+	 * This method traverses over the graph
+	 * @return
+	 */
+	public List traverse() {
+		return null;
+	}
+}
