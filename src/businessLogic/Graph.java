f83334220a2d3c5a9da56d41a67ecbcec2040ffe
camilotorresmestra <juctorresme@unal.edu.co>
2018-05-15 02:31:29 -0500

    Created all the methods for connections
    Created all the functions that manage the logic behind connections
    between nodes. Think there might be an easier way, perhaps unifying the
    Lists of the second type.
    It's still lacking a method for traverse the graph, and the logic behind
    weighted connections.

--- src/businessLogic/Graph.java
+++ src/businessLogic/Graph.java
@@ -1,12 +1,73 @@
 package businessLogic;
 
 public class Graph {
-	public List nodes;
+	public List users,articles,outfits;
+	public List wears, //from user to article or outfit
+				includes, //from outift to article
+				matches, //from article to article (Bidirectional)
+				liked, //from user to outfit or article
+				created; //from user to outfit
+	
 	/**
 	 * This method traverses over the graph
 	 * @return
 	 */
 	public List traverse() {
+		//TODO implement the graph traversing algorithm
 		return null;
+	}
+	
+	
+	public void addUser(User user) {
+		this.users.put(user);
+	}
+	
+	public void addArticle(Article article) {
+		this.articles.put(article);
+	}
+	
+	public void addOutfit(Outfit outfit) {
+		this.outfits.put(outfit);
+	}
+	
+	public void wears(User u, Article a) {
+		this.wears.put(new Edge(u,a,false));
+	}
+	
+	public void wears(User u, Outfit o) {
+		this.wears.put(new Edge(u,o,false));
+	}
+	
+	public void includes(Outfit o, Article a) {
+		this.includes.put(new Edge(o,a,false));
+	}
+	
+	public void matches(Article a1, Article a2) {
+		this.matches.put(new Edge(a1,a2,true));
+	}
+
+	
+	public void liked(User u, Outfit o) {
+		this.liked.put(new Edge(u,o,false));
+		
+		//If a user likes an outfit, it means she likes each of the articles in the outfit
+		
+		Edge temp = (Edge) o.outputEdges.head;
+		while(temp.next!=null) {
+			
+			liked(u,(Article) temp.oppositeNode(o));
+		}
+	}
+	
+	public void liked(User u, Article a) {
+		this.liked.put(new Edge(u,a,false));
+	}
+	
+	public void created(User u, Outfit o) {
+		this.created.put(new Edge(u,o,false));
+		
+		//If an user creates an article, it means that she likes it
+		
+		liked(u,o);
 	}
 }
